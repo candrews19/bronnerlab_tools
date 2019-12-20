7bdc3cd1d24f61416340cf112789a37f84e6ca1d
Cecelia Andrews <candrews@caltech.edu>
2019-12-19 22:27:05 -0800

    setup and init

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='bronnerlab_tools',
+    version='0.0.1',
+    url='https://github.com/candrews19/bronnerlab_tools.git',
+    description='Pip-installable package with functions used in hw9.1',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=read('requirements.txt').strip().split('\n'),
+)
