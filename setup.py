d7099425cfb9f966bdc367cc2b85e91f13305e7f
Cecelia Andrews <candrews@caltech.edu>
2019-12-19 22:28:05 -0800

    fixed setup

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     version='0.0.1',
     url='https://github.com/candrews19/bronnerlab_tools.git',
     description='Pip-installable package with functions used in hw9.1',
-    long_description=long_description,
     long_description_content_type='text/markdown',
     python_requires='>=3.6',
     license='MIT',
